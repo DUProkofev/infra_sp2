f7c47831e16222b9d8486f7e3aec1d4f802549bf
Прокофьев Денис Юрьевич <pdy@navigator.private>
2022-08-18 14:05:19 +0300

    add docker for web, composefile, conf for nginx

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -61,10 +61,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('POSTGRES_USER'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': os.getenv('DB_PORT'),
+		'USER': os.getenv('POSTGRES_USER'),
+		'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+		'HOST': os.getenv('DB_HOST'),
+		'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -98,10 +98,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
