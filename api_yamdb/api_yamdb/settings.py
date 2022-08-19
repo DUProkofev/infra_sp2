80391a3fa30195264818175a4852089e1606fb7c
Прокофьев Денис Юрьевич <pdy@navigator.private>
2022-08-19 15:05:22 +0300

    fix pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -61,10 +61,10 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('POSTGRES_USER'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
@@ -99,8 +99,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
