2baf761f4a73b26487db89fde7138ebf8e255d8a
Denis Prokofev <98609026+DUProkofev@users.noreply.github.com>
2022-08-20 16:45:56 +0300

    fix ya tests

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split()
 
 
 INSTALLED_APPS = [
