598a65982b9090f77f1d55e2207b1e8519ce52fe
duprokofev <heogame123@gmail.com>
2022-08-22 16:27:27 +0300

    fix pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = strtobool(os.getenv('DEBUG'))
+DEBUG = strtobool(os.getenv('DEBUG', default='True'))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
 
